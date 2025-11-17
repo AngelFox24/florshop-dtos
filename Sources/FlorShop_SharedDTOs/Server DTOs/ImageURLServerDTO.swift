3c3f751eb5270a63430e4ef75e89ef78d68b28c5
Angel Curi Laurente <curilaurente@gmail.com>
2025-11-16 22:00:05 -0500

    feat: image serve url

--- Sources/FlorShop_SharedDTOs/Server DTOs/ImageURLServerDTO.swift
+++ Sources/FlorShop_SharedDTOs/Server DTOs/ImageURLServerDTO.swift
@@ -1,20 +1,11 @@
 import Foundation
 
 public struct ImageURLServerDTO: Sendable, Codable {
-    public let imageCic: String?
-    public let imageUrl: String?
-    public let imageHash: String
-    public let imageData: Data?
+    public let imageData: Data
     
     public init(
-        imageCic: String?,
-        imageUrl: String?,
-        imageHash: String,
-        imageData: Data?
+        imageData: Data
     ) {
-        self.imageCic = imageCic
-        self.imageUrl = imageUrl
-        self.imageHash = imageHash
         self.imageData = imageData
     }
 }
