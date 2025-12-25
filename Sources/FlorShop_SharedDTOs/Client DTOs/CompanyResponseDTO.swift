b2d074103ea22bd1c6dff396940ff662babcc857
Angel Curi Laurente <curilaurente@gmail.com>
2025-12-25 17:00:37 -0500

    feat: optmize queries

--- Sources/FlorShop_SharedDTOs/Client DTOs/CompanyResponseDTO.swift
+++ Sources/FlorShop_SharedDTOs/Client DTOs/CompanyResponseDTO.swift
@@ -3,19 +3,16 @@ public struct CompanyResponseDTO: Sendable, Codable {
     public let name: String
     public let subdomain: String
     public let is_company_owner: Bool
-    public let subsidaries: [SubsidiaryResponseDTO]
     
     public init(
         company_cic: String,
         name: String,
         subdomain: String,
-        is_company_owner: Bool,
-        subsidaries: [SubsidiaryResponseDTO]
+        is_company_owner: Bool
     ) {
         self.company_cic = company_cic
         self.name = name
         self.subdomain = subdomain
         self.is_company_owner = is_company_owner
-        self.subsidaries = subsidaries
     }
 }
