d5a7b98d968b7e9d663f888b8ff392c797de086b
Angel Curi Laurente <curilaurente@gmail.com>
2026-02-17 12:21:52 -0500

    add support to rounded

--- Sources/FlorShop_SharedDTOs/Server DTOs/CartServerDTO.swift
+++ Sources/FlorShop_SharedDTOs/Server DTOs/CartServerDTO.swift
@@ -3,12 +3,15 @@ import Foundation
 public struct CartServerDTO: Sendable, Codable {
     public let cartDetails: [CartDetailServerDTO]
     public let total: Int
+    public let totalRounded: Int
     
     public init(
         cartDetails: [CartDetailServerDTO],
-        total: Int
+        total: Int,
+        totalRounded: Int
     ) {
         self.cartDetails = cartDetails
         self.total = total
+        self.totalRounded = totalRounded
     }
 }
