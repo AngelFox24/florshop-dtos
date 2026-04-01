2219bc81a34abd3492caa3e133480b66e29e0de7
Angel Curi Laurente <curilaurente@gmail.com>
2026-03-31 19:55:07 -0500

    add more params

--- Sources/FlorShop_SharedDTOs/Client DTOs/Response/CreateOrderResponse.swift
+++ Sources/FlorShop_SharedDTOs/Client DTOs/Response/CreateOrderResponse.swift
@@ -1,9 +1,21 @@
 public struct CreateOrderResponse: Sendable, Codable {
     public let orderId: String
+    public let planName: String
+    public let amount: Int
+    public let currency: String
+    public let email: String
     
     public init(
-        orderId: String
+        orderId: String,
+        planName: String,
+        amount: Int,
+        currency: String,
+        email: String
     ) {
         self.orderId = orderId
+        self.planName = planName
+        self.amount = amount
+        self.currency = currency
+        self.email = email
     }
 }
