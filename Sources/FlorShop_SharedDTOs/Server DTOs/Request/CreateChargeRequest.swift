b27f0d72e07405ec3edc6ea553c3e18db4b79695
Angel Curi Laurente <curilaurente@gmail.com>
2026-04-02 18:13:40 -0500

    modify charge dto

--- Sources/FlorShop_SharedDTOs/Server DTOs/Request/CreateChargeRequest.swift
+++ Sources/FlorShop_SharedDTOs/Server DTOs/Request/CreateChargeRequest.swift
@@ -1,15 +1,12 @@
 public struct CreateChargeRequest: Sendable, Codable {
-    public let planCic: String
-    public let companyCic: String
     public let tokenId: String
+    public let orderId: String
     
     public init(
-        planCic: String,
-        companyCic: String,
-        tokenId: String
+        tokenId: String,
+        orderId: String
     ) {
-        self.planCic = planCic
-        self.companyCic = companyCic
         self.tokenId = tokenId
+        self.orderId = orderId
     }
 }
