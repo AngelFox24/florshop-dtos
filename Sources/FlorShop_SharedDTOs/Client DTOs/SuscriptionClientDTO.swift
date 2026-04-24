62b77f763be2842468d4bd76f3b8ee9f8a53c260
Angel Curi Laurente <curilaurente@gmail.com>
2026-04-24 11:08:07 -0500

    add more detail to suscription in web

--- Sources/FlorShop_SharedDTOs/Client DTOs/SuscriptionClientDTO.swift
+++ Sources/FlorShop_SharedDTOs/Client DTOs/SuscriptionClientDTO.swift
@@ -3,12 +3,18 @@ import Foundation
 public struct SuscriptionClientDTO: Sendable, Codable {//DTO para web para mostrar los planes
     public let suscriptionCic: String
     public let suscriptionExpireAt: Date
+    public let planName: String
+    public let planPrice: Int
     
     public init(
         suscriptionCic: String,
-        suscriptionExpireAt: Date
+        suscriptionExpireAt: Date,
+        planName: String,
+        planPrice: Int
     ) {
         self.suscriptionCic = suscriptionCic
         self.suscriptionExpireAt = suscriptionExpireAt
+        self.planName = planName
+        self.planPrice = planPrice
     }
 }
