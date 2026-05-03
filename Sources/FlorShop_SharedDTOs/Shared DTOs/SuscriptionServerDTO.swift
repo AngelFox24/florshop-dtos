65374cc289fbaeb5b85d8457c47817a410b335f9
Angel Curi Laurente <curilaurente@gmail.com>
2026-05-03 09:35:11 -0500

    feat: add status in events

--- Sources/FlorShop_SharedDTOs/Shared DTOs/SuscriptionServerDTO.swift
+++ Sources/FlorShop_SharedDTOs/Shared DTOs/SuscriptionServerDTO.swift
@@ -5,16 +5,19 @@ public struct SuscriptionServerDTO: Sendable, Codable {//DTO para servicios inte
     public let companyCic: String
     public let suscriptionExpireAt: Date
     public let planCic: String
+    public let status: SubscriptionStatus
     
     public init(
         suscriptionCic: String,
         companyCic: String,
         suscriptionExpireAt: Date,
-        planCic: String
+        planCic: String,
+        status: SubscriptionStatus
     ) {
         self.suscriptionCic = suscriptionCic
         self.companyCic = companyCic
         self.suscriptionExpireAt = suscriptionExpireAt
         self.planCic = planCic
+        self.status = status
     }
 }
